d7174fc8daa4269c34f5f3bda74701388b1260b6
marwaalalawy <marwa.alalawy@gmail.com>
2017-02-13 20:24:38 +0200

    server

--- server/app.js
+++ server/app.js
@@ -1,4 +1,14 @@
+var express = require('express')
 var mongoose = require('mongoose');
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
 
 var mongoURI = process.env.MONGODB_URI ||'mongodb://localhost/WarshahDB';
 mongoose.connect(mongoURI);
