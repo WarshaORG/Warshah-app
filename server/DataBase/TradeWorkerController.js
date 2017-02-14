8d42d8f55ca9d07f918d8733dd6b48b996d1d260
Dua'a Maloalain <maloalain.duaa94@hotmail.com>
2017-02-14 20:20:15 +0200

    (feat) add insert function

--- server/DataBase/TradeWorkerController.js
+++ server/DataBase/TradeWorkerController.js
@@ -1,21 +1,20 @@
 var TradeWorker = require ('./TradeWorkerModel');
 
 module.exports = {
-	signin : function (req, res, next) {
+	insert : function (req, res) {
 		TradeWorker.findOne({username : req.body.username})
  			.exec(function (error, user) {
 	 			if(user){
-	 				next(new Error('TradeWorker already exist!'));
+	 				res.json(new Error('TradeWorker already exist!'));
 	 			}else{
 					var newTradeWorker = new TradeWorker ({
 						username : req.body.username,
-			        	password : req.body.password,
 			        	email:req.body.email,
 			        	place : req.body.place,
 			        	service : req.body.service,
 			        	phone : req.body.phone,
 			        	experiance : req.body.experiance,
-			        	picture: req.body.picture
+			        	picture: req.body.picture 
 					});
 					newTradeWorker.save(function(err, newTradeWorker){
 			    		if(err){
@@ -26,20 +25,20 @@ module.exports = {
 					});
 				}
 			})
-	},
+	}
 	
-	signup: function (req, res, next) {
-		var username = req.body.username;
-		var password = req.body.password;
-		// check to see if user already exists
-		findOne({username: req.body.username})
-		.then(function (TradeWorker) {
-			if (TradeWorker) {
-				new Error('TradeWorker already exist!');
-			}else{
-			// make a new TradeWorker if not one
-			signin();
-			}
-		})
-	},	
+	// signup: function (req, res, next) {
+	// 	var username = req.body.username;
+	// 	var password = req.body.password;
+	// 	// check to see if user already exists
+	// 	findOne({username: req.body.username})
+	// 	.then(function (TradeWorker) {
+	// 		if (TradeWorker) {
+	// 			new Error('TradeWorker already exist!');
+	// 		}else{
+	// 		// make a new TradeWorker if not one
+	// 		signin();
+	// 		}
+	// 	})
+	// }	
 }
