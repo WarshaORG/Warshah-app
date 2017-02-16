0bc6f5053e585be9caa00c0677cf5a9f42405d7e
Dua'a Maloalain <maloalain.duaa94@hotmail.com>
2017-02-16 17:03:13 +0200

    (feat) add functions

--- client/app/tradeworker/Tradeworker.js
+++ client/app/tradeworker/Tradeworker.js
@@ -1,22 +1,29 @@
  angular.module('myapp.TradeworkerContr',[])
 
-.controller('TradeworkerContr',function ($scope,$http,$location, Tradeworker){
+.controller('TradeworkerContr',function ($scope,$http,$location,Tradeworker){
 	$scope.tradeworker = {};
 	$scope.allTradeWorker = {};
 
 	$scope.getAllTradeworker = function(){
 		Tradeworker.getAll()
 		.then(function (data) {
-				
+			$scope.allTradeWorker = data;
 			console.log(data)
 		})
+		.catch(function (error) {
+			console.log(error);
+		}
 	}
 
 	$scope.addhandworker = function(){
 		Tradeworker.insert($scope.tradeworker)
 		.then(function (data) {
+		    $scope.allTradeWorker = data;
 			console.log(data);
 		})
+		.catch(function (error) {
+			console.log(error);
+		}
 	}
 
 })
